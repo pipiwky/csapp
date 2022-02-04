b2f1fb653a1e1afdc576631fbc76b8f0e0440e0c
pipiwky <wlkyyte@gmail.com>
2022-02-04 00:44:59 -0800

    ss

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,7 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <unistd.h>
+#include <getopt.h>
 #include "cachelab.h"
-
-int main()
+#include <string.h>
+void callprint()
 {
-    printSummary(0, 0, 0);
-    return 0;
+    printf("Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n"
+            "Options:\n"
+            "  -h         Print this help message.\n"
+            "  -v         Optional verbose flag.\n"
+            "  -s <num>   Number of set index bits.\n"
+            "  -E <num>   Number of lines per set.\n"
+            "  -b <num>   Number of block offset bits.\n"
+            "  -t <file>  Trace file.\n\n"
+            "Examples:\n"
+            "  linux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.trace\n"
+            "  linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
+}
+int main(int argc,char **argv)
+{
+    int opt,s,E,b;
+    char *t;
+    while((opt=getopt(argc,argv,"hvs:E:b:t:"))!=-1){
+        switch (opt)
+        {
+            case 'h':
+                callprint();
+                break;
+            case 'v':
+                callprint();
+                break;
+            case 's':
+                s = atoi(optarg);
+                break;
+            case 'E':
+                E = atoi(optarg);
+                break;
+            case 'b':
+                b = atoi(optarg);
+                break;
+            case 't':
+                t = strcpy(t,optarg);
+                break;
+        default:
+            printf("wrong argument\n");
+            break;
+        }
+        
+    }
+    printf("%d %d %d ",s,E,b);
+    
 }
