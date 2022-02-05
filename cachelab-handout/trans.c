3c6b796f3ea198de90f13f40b8e99fd63a207d3e
pipiwky <wlkyyte@gmail.com>
2022-02-05 00:23:24 -0800

    oss

--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -9,7 +9,10 @@
  */ 
 #include <stdio.h>
 #include "cachelab.h"
-
+int min(int a,int b){
+    if (a<= b) return a;
+    else return b;
+}
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
 /* 
@@ -22,6 +25,94 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    if(M==32&&N==32){
+        int a0,a1,a2,a3,a4,a5,a6,a7;
+        int i,j,k;
+        for (i = 0; i < 32; i += 8) {
+            for (j = 0; j < 32; j += 8) {
+                for (k = i; k < i + 8; k++) {
+                    a0 = A[k][j];
+                    a1 = A[k][j + 1];
+                    a2 = A[k][j + 2];
+                    a3 = A[k][j + 3];
+                    a4 = A[k][j + 4];
+                    a5 = A[k][j + 5];
+                    a6 = A[k][j + 6];
+                    a7 = A[k][j + 7];
+                    B[j][k] = a0;
+                    B[j + 1][k] = a1;
+                    B[j + 2][k] = a2;
+                    B[j + 3][k] = a3;
+                    B[j + 4][k] = a4;
+                    B[j + 5][k] = a5;
+                    B[j + 6][k] = a6;
+                    B[j + 7][k] = a7;
+                }
+            }
+        }
+    }
+    if(M == 64&&N==64){
+        const int len = 4;
+        int a0,a1,a2,a3;
+        int i,j,k,s;
+        for (i = 0; i < N; i += len) {
+            for (j = 0; j < N; j += len) {
+                // copy
+                for (k = i, s = j; k < i + len; k++, s++) {
+                    a0 = A[k][j];
+                    a1 = A[k][j + 1];
+                    a2 = A[k][j + 2];
+                    a3 = A[k][j + 3];
+                    B[s][i] = a0;
+                    B[s][i + 1] = a1;
+                    B[s][i + 2] = a2;
+                    B[s][i + 3] = a3;
+                }
+                // transpose
+                for (k = 0; k < len; k++) {
+                    for (s = k + 1; s < len; s++) {
+                        a0 = B[k + j][s + i];
+                        B[k + j][s + i] = B[s + j][k + i];
+                        B[s + j][k + i] = a0;
+                    }
+                }
+            }
+        }
+    } // for full marks there are some implementations in the internet for reference
+    if(M == 61&&N==67){
+        int i,j,k,s;
+        int a0,a1,a2,a3,a4,a5,a6,a7;
+        for (i = 0; i < N; i += 8) {
+            for (j = 0; j < M; j += 23) {
+                if (i + 8 <= N && j + 23 <= M) {
+                    for (s = j; s < j + 23; s++) {
+                        a0 = A[i][s];
+                        a1 = A[i + 1][s];
+                        a2 = A[i + 2][s];
+                        a3 = A[i + 3][s];
+                        a4 = A[i + 4][s];
+                        a5 = A[i + 5][s];
+                        a6 = A[i + 6][s];
+                        a7 = A[i + 7][s];
+                        B[s][i + 0] = a0;
+                        B[s][i + 1] = a1;
+                        B[s][i + 2] = a2;
+                        B[s][i + 3] = a3;
+                        B[s][i + 4] = a4;
+                        B[s][i + 5] = a5;
+                        B[s][i + 6] = a6;
+                        B[s][i + 7] = a7;
+                    }
+                } else {
+                    for (k = i; k < min(i + 8, N); k++) {
+                        for (s = j; s < min(j + 23, M); s++) {
+                            B[s][k] = A[k][s];
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 /* 
